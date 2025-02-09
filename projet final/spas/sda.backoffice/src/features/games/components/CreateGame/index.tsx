6d27e2d044eb73c2aac3d9de09c69a09ea041ffc
Pierre Berkovits <berkovitspierre@gmail.com>
2025-02-09 17:24:24 +0100

    sec 15/140

--- projet final/spas/sda.backoffice/src/features/games/components/CreateGame/index.tsx
+++ projet final/spas/sda.backoffice/src/features/games/components/CreateGame/index.tsx
@@ -2,16 +2,21 @@ import { SubmitHandler, useForm } from "react-hook-form"
 import { FormInput } from "./models"
 
 export const CreateGame = () => {
-    const {register, handleSubmit} = useForm<FormInput>()
+    const {register, handleSubmit, formState: { errors, isValid }} = useForm<FormInput>()
     const onSubmit: SubmitHandler<FormInput> = data => console.info(data)
 
+    let titleStyle = {
+        backgroundColor: 'white',
+        color: 'blue',
+        borderColor: errors.title ? 'red': 'inherit'
+    }
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register('title')}></input>
-                <button>Sauvegarder</button>
+                <input {...register('title', { required: true })} style = { titleStyle }></input>
+                { errors.title && <span style={{color: 'red'}}>*</span>}
+                <button disabled={! isValid}>Sauvegarder</button>
             </form>
         </>
     )
-
 }
